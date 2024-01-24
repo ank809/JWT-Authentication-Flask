e25dde9ce4cae2a5517a09d8507f0246cfbf0017
ank809 <ankita51523@gmail.com>
2024-01-24 22:13:16 +0530

    error handling used

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,16 @@ app.config['JWT_SECRET_KEY']=JWT_SECRET_KEY
 db=PyMongo(app).db
 jwt= JWTManager(app)
 
-from app.routes import auth, user 
+@jwt.expired_token_loader
+def expired_token_callback(jwt_header, jwt_data):
+    return jsonify({"error":"Token has been expired"}),401
 
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return jsonify({"error":"Invalid token"}),401
 
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return jsonify({"Error":"Request does not contain valid token", "error":"Unauthorized token"}),401
+
+from app.routes import user, auth
--- app/routes/user.py
+++ app/routes/user.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from app import db, app
 
